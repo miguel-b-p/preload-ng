42ef4bfe44a6e1c087801e44f6be8c99f318d3df
Mingas <miguelpinotty@gmail.com>
2025-12-09 00:40:27 -0300

    refactor: Reorganize source files into `utils`, `config`, and `daemon` subdirectories.

--- preload-src/src/prophet.h
+++
@@ -1,7 +0,0 @@
-#ifndef PROPHET_H
-#define PROPHET_H
-
-void preload_prophet_predict (gpointer data);
-void preload_prophet_readahead (GPtrArray *maps_arr);
-
-#endif
--- preload-src/src/readahead.h
+++
@@ -1,8 +0,0 @@
-#ifndef READAHEAD_H
-#define READAHEAD_H
-
-#include <state.h>
-
-int preload_readahead (preload_map_t **files, int file_count);
-
-#endif
